1a2cd1d791fa531179309d3dfc62f9354c05dedb
BmeUP <russkiyya52@gmail.com>
2021-11-21 21:49:20 +0300

    init2

--- eng_gls_frontend/eng_gls_backend/eng_gls/m.py
+++
@@ -1,4 +0,0 @@
-import os
-
-x = os.environ.get('DB_USER')
-print(x)
--- eng_gls_frontend/eng_gls_backend/glossary_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Glossary, Word, Translation, WordInGlossary
-
-models_lst = [Glossary, Word, Translation, WordInGlossary]
-
-admin.site.register(models_lst)

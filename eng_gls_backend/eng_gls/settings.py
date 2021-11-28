ff714ed04495e36d04d31f736556a6d78530f0f2
BmeUP <russkiyya52@gmail.com>
2021-11-28 20:25:24 +0300

    test actions

--- eng_gls_backend/eng_gls/settings.py
+++ eng_gls_backend/eng_gls/settings.py
@@ -25,7 +25,7 @@ config = dotenv_values(BASE_DIR/".env")
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = False # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True

1e72abba8608bb94bfe693ef813fa0a891626a84
BmeUP <russkiyya52@gmail.com>
2021-11-28 21:06:27 +0300

    test actions

--- eng_gls_backend/eng_gls/settings.py
+++ eng_gls_backend/eng_gls/settings.py
@@ -23,10 +23,10 @@ config = dotenv_values(BASE_DIR/".env")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = config.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CORS_ORIGIN_ALLOW_ALL = False # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True

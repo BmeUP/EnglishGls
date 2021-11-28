0d3c463c1a381ff10cda2146a56b7f808cb4770c
BmeUP <russkiyya52@gmail.com>
2021-11-28 15:55:41 +0300

    test actions

--- eng_gls_backend/eng_gls/ci_env.py
+++ eng_gls_backend/eng_gls/ci_env.py
@@ -1,5 +1,4 @@
 class Sercete:
-    SECRET_KEY = 'ci_secrete'
     DB_NAME='ci_db'
     DB_USER='ci_user'
     DB_PASSWORD='ci_password'
--- eng_gls_backend/eng_gls/settings.py
+++ eng_gls_backend/eng_gls/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
-
+from django.core.management.utils import get_random_secret_key
 try:
     from .env import Sercete
 except ModuleNotFoundError:
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Sercete.SECRET_KEY
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
